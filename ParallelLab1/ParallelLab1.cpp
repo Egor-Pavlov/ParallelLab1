215789174f9f3d301e8507269d6a0339559cf029
Егор <EgorPavlov2001@yandex.ru>
2021-07-31 21:00:42 +0700

    вроде робит как надо но странно

--- ParallelLab1/ParallelLab1.cpp
+++ ParallelLab1/ParallelLab1.cpp
@@ -6,12 +6,13 @@
 #include <chrono>
 #include <Windows.h>
 #include <pthread.h>
+#include <fstream>
 
 using namespace std;
 typedef double(*pointFunc)(double);
 
 int countOfThr;
-double a = 1, b = 100, eps = 0.01, h;//Нижний и верхний пределы интегрирования (a, b), погрешность (eps).
+double a = 0, b = 75, eps = 0.00001, h;//Нижний и верхний пределы интегрирования (a, b), погрешность (eps).
 double iMax;
 int N;//кол-во вычислений на данной итерации внешнего цикла
 
@@ -23,7 +24,7 @@ struct Thread
 
 double f(double x)
 {
-	Sleep(1);//имитация сложных вычислений :D
+	Sleep(0.9);//имитация сложных вычислений :D
 	return 10-x;
 }
 
@@ -77,33 +78,51 @@ int main()
 	setlocale(LC_ALL, "ru");
 	double s1, s;
 	N = 1; //начальное число шагов
-	cout << "Введите левую границу интегрирования a = ";
-	cin >> a;
-	cout << "\nВведите правую границу интегрирования b = ";
-	cin >> b;
-	cout << "\nВведите требуемую точность eps = ";
-	cin >> eps;
-	cout << "\nВведите кол-во потоков: " ;
-	cin >> countOfThr;
+	int MaxCount = 8, MinCount = 1;
+	vector<pair<int, double>> Rezults;
 
-	//начало отсчета времени вычисления
-	auto startTime = std::chrono::high_resolution_clock::now();
+	//cout << "Введите левую границу интегрирования a = ";
+	//cin >> a;
+	//cout << "\nВведите правую границу интегрирования b = ";
+	//cin >> b;
+	//cout << "\nВведите требуемую точность eps = ";
+	//cin >> eps;
+	//cout << "\nВведите минимальное число потоков для эксперимента ";
+	//cin >> MinCount;
+	cout << "\nВведите максимальное число потоков для эксперимента ";
+	cin >> MaxCount;
+	//
+	//for (int i = MinCount; i <= MaxCount; i++)
+	//{
+		countOfThr = MaxCount;
+		//начало отсчета времени вычисления
+		auto startTime = std::chrono::high_resolution_clock::now();
 
-	s1 = simpson_integral(); //первое приближение для интеграла
-	do {
-		s = s1;     //второе приближение
-		N = 2 * N;  //увеличение числа шагов в два раза,
-					//т.е. уменьшение значения шага в два раза
-		s1 = simpson_integral();
+		s1 = simpson_integral(); //первое приближение для интеграла
+		do {
+			s = s1;     //второе приближение
+			N = 2 * N;  //увеличение числа шагов в два раза,
+						//т.е. уменьшение значения шага в два раза
+			s1 = simpson_integral();
 
-	}   while (fabs(s1 - s) > eps);  //сравнение приближений с заданной точностью
+		} while (fabs(s1 - s) > eps);  //сравнение приближений с заданной точностью
 
-	//окончание отсчета времениcicleLen
-	auto endTime = std::chrono::high_resolution_clock::now() - startTime;
-	//подсчет времени вычисления
-	double elapseTime = std::chrono::duration<double>(endTime).count();
-
+		//окончание отсчета времениcicleLen
+		auto endTime = std::chrono::high_resolution_clock::now() - startTime;
+		//подсчет времени вычисления
+		double elapseTime = std::chrono::duration<double>(endTime).count();
+	//	Rezults.push_back(make_pair(i, elapseTime));
+	//}
+	//ofstream fout("Lab1Rez.xls"); // создаём объект класса ofstream для записи
+	//fout << "Количество потоков\t Время работы" << endl;
+	//for (int i = 0; i < Rezults.size(); i++)
+	//{
+	//	fout << Rezults[i].first << "\t" << Rezults[i].second << endl;
+	//}
+	//
+	//fout.close(); // закрываем файл
 	cout << "Значение интеграла: " << s1 << endl;
-	cout << "Время вычисления: " << elapseTime;
+	cout << "time " << elapseTime;
+
 	return 0;
 }
